bf03fa6f432416a55a9f2be712d55919b786457e
Thanakorn Waleejaroenpong <tanakorn_wal@cmu.ac.th>
2023-10-09 23:58:07 +0700

    2.7

--- 331-backend/src/main/java/se331/lab/rest/controller/BucketController.java
+++ 331-backend/src/main/java/se331/lab/rest/controller/BucketController.java
@@ -19,4 +19,8 @@ public class BucketController {
     public ResponseEntity<?> uploadFile(@RequestPart(value = "file")MultipartFile file) throws IOException, ServletException {
         return ResponseEntity.ok(this.cloudStorageHelper.getImageUrl(file, "imageupload-cb744.appspot.com"));
     }
+    @PostMapping("/uploadImage")
+    public ResponseEntity<?> uploadFileComponent(@RequestPart(value = "image")MultipartFile file) throws IOException, ServletException {
+        return ResponseEntity.ok(this.cloudStorageHelper.getStorageFileDto(file, "imageupload-cb744.appspot.com"));
+    }
 }
